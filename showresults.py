adf93d2eff9e4ffb3e11d9d0796b0ce94748a06c
Brendon J. Brewer <brendon.brewer@gmail.com>
2026-05-02 14:52:41 +1200

    Fixes

--- showresults.py
+++ showresults.py
@@ -1,14 +1,14 @@
 import numpy as np
 
 logzs  = np.atleast_1d(np.loadtxt("logzs.txt"))
-logzs2 = np.atleast_2d(np.loadtxt("logzs2.txt"))
+logzs2 = np.atleast_1d(np.loadtxt("logzs2.txt"))
 
 # Truncate to equal length
 logzs = logzs[0:len(logzs2)]
 
 # Calculate entropy
 diffs = logzs - logzs2
-print("Number of runs =", len(diffs))
+print(f"Number of runs = {len(diffs)}.")
 H = np.mean(diffs)
-sem = np.std(diffs)/np.sqrt(len(diffs))
+sem = np.std(diffs, ddof=1)/np.sqrt(len(diffs))
 print(f"H = {H} +- {sem}.")
