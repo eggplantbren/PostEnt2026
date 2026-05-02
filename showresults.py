a1284889e46089f8fb26a6ca7f4c8810f5723493
Brendon J. Brewer <brendon.brewer@gmail.com>
2026-05-02 13:07:47 +1200

    Compute H but it's wrong

---
+++ showresults.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+logzs = np.loadtxt("logzs.txt")
+logzs2 = np.loadtxt("logzs2.txt")
+
+# Truncate to equal length
+logzs = logzs[0:len(logzs2)]
+
+# Calculate entropy
+diffs = logzs - logzs2
+print("Number of runs =", len(diffs))
+H = np.mean(diffs)
+sem = np.std(diffs)/np.sqrt(len(diffs))
+print(f"H = {H} +- {sem}.")
